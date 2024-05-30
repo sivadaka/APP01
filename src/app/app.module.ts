63941aee422d51cfb9d3b3568cdd95649b8db483
Siva Daka <127689865+sivadaka@users.noreply.github.com>
2024-05-30 22:01:22 +0530

    gallery feature initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { FormsModule } from '@angular/forms';
 import { ArthComponent } from './arth/arth.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
-    ArthComponent
+    ArthComponent,
+    GalleryComponent
   ],
   imports: [
     BrowserModule,
