4391e099934425da0b0e27e3fb54bcfba06b1123
Siva Daka <127689865+sivadaka@users.noreply.github.com>
2024-05-31 18:03:18 +0530

    friends assignment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { FormsModule } from '@angular/forms';
 import { ArthComponent } from './arth/arth.component';
 import { GalleryComponent } from './gallery/gallery.component';
+import { FriendsComponent } from './friends/friends.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     WelcomeComponent,
     ArthComponent,
-    GalleryComponent
+    GalleryComponent,
+    FriendsComponent
   ],
   imports: [
     BrowserModule,
